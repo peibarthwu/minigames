b23f0491b5bceb2d7a6a491f2d0137a133758333
Pei Pei Barth Wu <peibarthwu@gmail.com>
2021-07-14 18:15:14 +0100

    controls

--- Assets/Scripts/dotgame.cs
+++ Assets/Scripts/dotgame.cs
@@ -9,49 +9,79 @@ public class dotgame : MonoBehaviour
     public int difficulty;
 
     private LineRenderer lr;
-    private GameObject[] sphereObjects = new GameObject[9];
+
+    private List<GameObject> sphereObjects = new List<GameObject>();
     
     private List<Vector3> userPoints = new List<Vector3>();
     private List<Vector3> points = new List<Vector3>();
+    private Vector3[] isoverts;
 
-    // private Vector3[] points;
-    // private Vector3[] userPoints;
 
     private Vector3 firstPoint;
     private Vector3 secondPoint;
     private int check = 0;
 
-    int numRows = 3;
-    int numCols = 3;
-    int space = 2;
+   
+    // int space = 2;
 
+    private Vector3[] IcosahedronVertices()
+    {
+            float t = (1f + Mathf.Sqrt(5f)) / 2f;
+        
+            return new Vector3[]
+            {
+                new Vector3(-1,  t,  0),
+                new Vector3( 1,  t,  0),
+                new Vector3(-1, -t,  0),
+                new Vector3( 1, -t,  0),
+                new Vector3( 0, -1,  t),
+                new Vector3( 0,  1,  t),
+                new Vector3( 0, -1, -t),
+                new Vector3( 0,  1, -t),
+                new Vector3( t,  0, -1),
+                new Vector3( t,  0,  1),
+                new Vector3(-t,  0, -1),
+                new Vector3(-t,  0,  1)
+            };
+    }
     
 
 
     void Awake()
     {
-        int count = 0;
-        for (int i = 0; i < numRows; i++)
-            {
-                for (int j = 0; j < numCols; j++)
-                    {
-                        sphereObjects[count] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                        sphereObjects[count].name = "Sphere" + "_" + i + "_" + j;
-                        sphereObjects[count].transform.position = new Vector3(i * space, j * space, 0);
-                        sphereObjects[count].GetComponent<Renderer>().material = orbMat;
-                        count ++;
-                    }
-            }    
+        // int count = 0;
+        // for (int i = 0; i < numRows; i++)
+        //     {
+        //         for (int j = 0; j < numCols; j++)
+        //             {
+        //                 sphereObjects[count] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //                 sphereObjects[count].name = "Sphere" + "_" + i + "_" + j;
+        //                 sphereObjects[count].transform.position = new Vector3(i * space, j * space, 0);
+        //                 sphereObjects[count].GetComponent<Renderer>().material = orbMat;
+        //                 count ++;
+        //             }
+        //     }  
+        isoverts = IcosahedronVertices();
+        for (int i = 0; i < isoverts.Length; i++)
+        {
+            var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphereObjects.Add(obj);
+            obj.name = "Sphere" + "_" + i;
+            obj.transform.position = isoverts[i];
+            obj.GetComponent<Renderer>().material = orbMat;
+        }
+
     }
 
     void DrawLine(Vector3 start, Vector3 end)
     {
         lr = (new GameObject("line")).AddComponent<LineRenderer>();
         
-        // lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
-        lr.SetWidth(0.5f, 0.5f);
+        lr.material = orbMat;
         lr.SetPosition(0, start);
         lr.SetPosition(1, end);
+        lr.startWidth = 0.3f;
+        lr.endWidth = 0.3f;
 
         userPoints.Add(firstPoint);
 
@@ -68,7 +98,7 @@ public class dotgame : MonoBehaviour
     {   
         for (int i = 0; i < difficulty; i++)
         {
-            var chosen = sphereObjects[Random.Range(0,sphereObjects.Length - 1)];
+            var chosen = sphereObjects[Random.Range(0,sphereObjects.Count - 1)];
             points.Add(chosen.transform.position);
 
             //Get the Renderer component for sphere
